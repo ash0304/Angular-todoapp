750895ce6572fea62f1afa9a4bfacfb8c406e4a4
Charles <z0975201508@gmail.com>
2018-12-07 18:26:04 +0800

    Angular todo app (1/3)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AddFormComponent } from './add-form/add-form.component';
+import { TodoItemsComponent } from './todo-items/todo-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddFormComponent,
+    TodoItemsComponent
   ],
   imports: [
     BrowserModule,
